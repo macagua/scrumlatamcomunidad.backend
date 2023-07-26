c9412985db79a23bbe33c97d03f77e1a98ef236d
Manuel Matos <manuelematosp@outlook.com>
2023-07-25 23:19:04 -0400

    Added more improvements about roles, roles mapping and it's testing #33

--- backend/src/slc_web/src/slc_web/tests/test_ct_community_sponsor.py
+++ backend/src/slc_web/src/slc_web/tests/test_ct_community_sponsor.py
@@ -2,7 +2,11 @@
 from plone.app.testing import setRoles
 from plone.app.testing import TEST_USER_ID
 from plone.dexterity.interfaces import IDexterityFTI
+
+from slc_web import ADD_SPONSOR_ROLES
+from slc_web import MANAGE_SPONSOR_ROLES
 from slc_web.testing import SLC_WEB_INTEGRATION_TESTING
+
 from zope.component import queryUtility
 
 import unittest
@@ -22,7 +26,47 @@ class ContentTypesTest(unittest.TestCase):
         self.portal = self.layer["portal"]
         setRoles(self.portal, TEST_USER_ID, ["Manager"])
 
-    def test_fti_community_sponsor(self):
+    def test_ct_community_sponsor_fti(self):
+        """Test If CommunitySponsor exits on Dexterity FTI."""
         fti = queryUtility(IDexterityFTI, name="CommunitySponsor")
         self.assertTrue(fti)
         # self.assertNotEqual(None, fti)
+
+    def test_ct_community_sponsor_globally_addable(self):
+        """Test If CommunitySponsor is globally addable."""
+
+        setRoles(self.portal, TEST_USER_ID, ["Contributor"])
+        fti = queryUtility(IDexterityFTI, name="CommunitySponsor")
+        self.assertTrue(
+            fti.global_allow,
+            "{0} is not globally addable!".format(fti.id),
+        )
+
+    def test_ct_community_sponsor_roles(self):
+        """Test correct assigning of permissions from rolemap.xml file."""
+
+        # Test permission mapping for adding an Community Sponsor
+        sponsor_roles = [
+            r["name"]
+            for r in self.portal.rolesOfPermission(
+                "Community Sponsor: Add Community Sponsor"
+            )
+            if r["selected"]
+        ]
+        self.assertEqual(sponsor_roles, ADD_SPONSOR_ROLES)
+
+        # Test permission mapping for View Detail
+        sponsor_roles = [
+            r["name"]
+            for r in self.portal.rolesOfPermission("Community Sponsor: View Detail")
+            if r["selected"]
+        ]
+        self.assertEqual(sponsor_roles, MANAGE_SPONSOR_ROLES)
+
+        # Test permission mapping for managing Manage Sponsors
+        sponsor_roles = [
+            r["name"]
+            for r in self.portal.rolesOfPermission("Community Sponsor: Manage Sponsors")
+            if r["selected"]
+        ]
+        self.assertEqual(sponsor_roles, MANAGE_SPONSOR_ROLES)
